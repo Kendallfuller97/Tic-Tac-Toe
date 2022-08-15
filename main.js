0e8ea49fbc27d40dce299f99587da25ef9544cf3
Kendall Fuller <kendallfuller97@yahoo.com>
2022-08-15 16:48:15 -0700

    docs (code) updated code

--- main.js
+++ main.js
@@ -1,17 +1,33 @@
 const X_IMAGE_URL = 'https://s3-us-west-2.amazonaws.com/s.cdpn.io/1083533/x.png';
 const O_IMAGE_URL = 'https://s3-us-west-2.amazonaws.com/s.cdpn.io/1083533/circle.png';
 
-function onClick(event) {
-  // Element that was clicked
-  const element = event.target;
-  // Create an <img> tag with the X img src
+function changeToX(event) {
+  const container = event.currentTarget;
   const image = document.createElement('img');
   image.src = X_IMAGE_URL;
-  // Append that <img> tag to the element
-  element.appendChild(image);
+  container.appendChild(image);
+  container.removeEventListener('click', changeToX);
+  computerChooseO();
 }
 
-const gridItems = document.querySelector('#grid div');
-for (const item of gridItems) {
-  item.addEventListener('click', onClick);
+function computerChooseO() {
+  const allBoxes  = document.querySelectorAll('#grid div');
+  const freeBoxes = [];
+  for (const box of allBoxes) {
+    let imageChild = box.querySelector('img');
+    if (!imageChild) {
+      freeBoxes.push(box);
+    }
+  }
+  const index = Math.floor(Math.random() * freeBoxes.length);
+  const freeSpace = freeBoxes[index];
+  const image = document.createElement('img');
+  image.src = O_IMAGE_URL;
+  freeSpace.removeEventListener('click', changeToX);
+  freeSpace.appendChild(image);
+}
+
+const boxes = document.querySelectorAll('#grid div');
+for (const box of boxes) {
+  box.addEventListener('click', changeToX);
 }
